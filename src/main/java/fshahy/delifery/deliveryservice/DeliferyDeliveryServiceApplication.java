7d412c57228a6d289e9872268df31bf9f13e91aa
Farid Shahy <fshahy@gmail.com>
2020-11-12 18:55:54 +0330

    enable service discovery.

--- src/main/java/fshahy/delifery/deliveryservice/DeliferyDeliveryServiceApplication.java
+++ src/main/java/fshahy/delifery/deliveryservice/DeliferyDeliveryServiceApplication.java
@@ -2,8 +2,10 @@ package fshahy.delifery.deliveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DeliferyDeliveryServiceApplication {
 
 	public static void main(String[] args) {
