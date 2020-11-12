d91ce7533e8c39db45776edec163d7f14ccc0007
Farid Shahy <fshahy@gmail.com>
2020-11-12 18:19:44 +0330

    init maven project.

---
+++ src/main/java/fshahy/delifery/deliveryservice/DeliferyDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.deliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliferyDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliferyDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fshahy/delifery/deliveryservice/DeliferyDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fshahy.delifery.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliferyDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
